e1aec7f095d0d5a149f507d8f31151637c2433a0
hejzbh <tulumovichazim18@gmail.com>
2025-02-25 21:10:04 +0100

    remove unused

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,3 @@ export const DESC_MAX_LENGTH = 500;
 export const EMAIL_MAX_LENGTH = 50;
 export const PW_MAX_LENGTH = 50;
 export const PW_MIN_LENGTH = 8;
-
-// auth
-export const ACCESS_TOKEN_DURATION = process.env.ACCESS_TOKEN_DURATION as any;
-export const REFRESH_TOKEN_DURATION = process.env.REFRESH_TOKEN_DURATION as any;
